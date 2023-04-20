460d5ca5eab25e5fe1d5fdd9ee7e71f52d609542
Aashish1799 <130171760+Aashish1799@users.noreply.github.com>
2023-04-20 11:18:33 +0530

    Create Reverse a number.c

---
+++ Reverse a number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int reverseNum(int *n){
+      int t;
+      t=*n;
+      int rem, rev=0;
+      while(t>0){
+              rem-t%10;
+              rev=rev*10 + rem;
+              t/=10;
+              return rev;
+int main() {
+int nl,a;
+printf("Enter the digit: \n");
+scanf("%d", &n1);
+a=reverseNum(&n1);
+printf("Reverse of the given digit is: ",a);"
+return 0;
